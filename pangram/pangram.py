c8d982225d570b89ef5fba200ffbd9849d4ab750
Angus Li <angusr.li02@gmail.com>
2021-05-08 14:44:36 +0800

    Added pangram v2

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -6,6 +6,4 @@ def is_pangram(sentence):
     alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
                 'n','o','p','q','r','s','t','u','v','w','x','y','z'}
     sentence_chars = {char for char in sentence.lower() if char in alphabet}
-    if sentence_chars == alphabet:
-        return True
-    return False
+    return sentence_chars == alphabet
