6a408d4355a683816b884cdbcbf3c1b8ce4aca20
Angus Li <angusr.li02@gmail.com>
2021-05-12 15:57:49 +0800

    Added matrix v4

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,13 +1,21 @@
 class Matrix:
-    def __init__(self, matrix_string):
+    def __init__(self, matrix_string: str) -> None:
         '''
-        matrix_string: string of numbers, "\n" denotes row break
+        A 2D array of integers, where each element represents a row. 
+        
+        Rows are split according to the delimiter "\n" in matrix_string.
         '''
-        rows = matrix_string.split("\n")
-        self.matrix = [list(map(int, row.split(" "))) for row in rows]
+        rows = matrix_string.splitlines()
+        self.matrix = [[int(entry) for entry in row.split()] for row in rows]
 
-    def row(self, index):
+    def row(self, index: int) -> list:
+        '''
+        Returns the nth row of the matrix, where n == index.
+        '''
         return self.matrix[index - 1]
 
-    def column(self, index):
+    def column(self, index: int) -> list:
+        '''
+        Returns a list of the nth integer in each row, where n == index.
+        '''
         return [row[index - 1] for row in self.matrix]
