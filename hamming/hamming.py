a6a8cca895e410ad33ece4ca0a0663f2c40dbb70
Angus Li <angusr.li02@gmail.com>
2021-06-15 18:01:21 +0800

    Added hamming v2.0

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,10 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands must be of equal length.")
-    
-    hamming_dist = 0
-    for char_a, char_b in zip(strand_a, strand_b):
-        if char_a != char_b:
-            hamming_dist += 1
-    
-    return hamming_dist
+    return sum(1 for char_a, char_b in zip(strand_a, strand_b) if char_a != char_b)
