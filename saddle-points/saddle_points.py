478bf2b7e53d5dc108605ae4d00cd55702886753
Angus Li <angusr.li02@gmail.com>
2021-07-08 22:41:43 +0800

    added saddle_points v2

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -6,30 +6,14 @@ def saddle_points(matrix: list) -> List[Dict[str, int]]:
     Return all entries which are largest in their row and are smallest in their
     column.
     '''
-    if len(matrix) > 0 and any(len(row) != len(matrix[0]) for row in matrix):
+    if len(set(map(len, matrix))) > 1:
         raise ValueError("Rows must all be of equal length.")
 
-    def get_row_index(value: int) -> List[int]:
-        '''
-        Return indices of all entries equal to value in row.
-        '''
-        return [index + 1 for index, x in enumerate(row) if x == value]
+    points = set()
 
-    def get_column_min(index: List[int]) -> List[int]:
-        '''
-        Return the smallest entry in the inth column.
-        '''
-        return min([row[index - 1] for row in matrix])
+    for i, row in enumerate(matrix):
+        for j, x in enumerate(row):
+            if x == max(row) and x == min([row[j] for row in matrix]):
+                points.add((i + 1, j + 1))
 
-    point_list = []
-
-    for row_num, row in enumerate(matrix, 1):
-        row_highest = max(row)
-        column_num = get_row_index(row_highest)
-        for num in column_num:
-            column_min = get_column_min(num)
-            if row_highest == column_min:
-                saddle_point = {"row": row_num, "column": num}
-                point_list.append(saddle_point)
-
-    return point_list
+    return [{"row": point[0], "column": point[1]} for point in points]
