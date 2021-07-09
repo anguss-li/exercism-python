e5abeb2d614816169b376a7a5186983051abc93d
Angus Li <angusr.li02@gmail.com>
2021-07-09 22:55:29 +0800

    Added isogram v1

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
-def is_isogram(string):
-    pass
+from string import ascii_lowercase
+
+
+def is_isogram(string: str) -> bool:
+    '''Check if all letters in string are unrepeated'''
+    word = string.lower()
+    return not any(word.count(char) > 1 and char in ascii_lowercase for char in word)
