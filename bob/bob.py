fa948fe50d71e6bd0a58565f0dc8cb9f50a0b7b1
Angus Li <angusr.li02@gmail.com>
2021-07-10 23:29:35 +0800

    Added bob v2

--- bob/bob.py
+++ bob/bob.py
@@ -1,20 +1,14 @@
-from string import ascii_uppercase as capitals, ascii_letters as letters
-
-
 def response(hey_bob: str) -> str:
     '''Emulate Bob's response according to the README'''
-    chars = hey_bob.strip()
-    is_capitalized = (all(char in capitals for char in chars if char in letters)
-                      and any(char in letters for char in chars))
-    is_question = chars.endswith("?")
-    is_silence = chars == ""
-
-    if is_silence:
-        return "Fine. Be that way!"
-    elif is_capitalized and is_question:
+    speech = hey_bob.strip()
+    is_capitalized = speech.isupper()
+    is_question = speech.endswith("?")
+    if is_capitalized and is_question:
         return "Calm down, I know what I'm doing!"
     elif is_capitalized:
         return "Whoa, chill out!"
     elif is_question:
         return "Sure."
+    elif speech == "":
+        return "Fine. Be that way!"
     return "Whatever."
