d4aa896b697040888d72d8a2a9e2ab55b4e90710
Angus Li <angusr.li02@gmail.com>
2021-07-10 20:56:20 +0800

    Added documentation to palindrome_products

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -7,9 +7,15 @@ def get_factors(n: int, min_factor: int, max_factor: int) -> List[List[int]]:
     '''
     if n == None:
         return []
-    root = int(n ** 0.5)
-    lower = min_factor if min_factor > n // max_factor else n // max_factor
-    upper = (max_factor if max_factor < root else root) + 1
+
+    # For a integer i in range(min_factor, max_factor) to be a valid factor of 
+    # n, n//i <= max_factor must be True. This evaluates to i >= n//max_factor.
+    min_i = n // max_factor
+    lower = min_factor if min_factor > min_i else min_i
+
+    square_root = int(n ** 0.5)
+    upper = (max_factor if max_factor < square_root else square_root) + 1
+
     return [[i, n // i] for i in range(lower, upper) if n % i == 0]
 
 
@@ -20,7 +26,7 @@ def get_palindrome(lower: int, upper: int, reverse: bool = False) -> int:
     '''
     if lower > upper:
         raise ValueError("Minimum larger than maximum")
-    args = (upper ** 2, lower ** 2 - 1, -1) if reverse else (lower ** 2, upper ** 2 + 1)
+    args = (upper ** 2, lower**2 - 1, -1) if reverse else (lower ** 2, upper**2 + 1)
     for x in range(*args):
         x_str = str(x)
         if x_str == x_str[::-1] and any(upper >= f[1] for f in get_factors(x, lower, upper)):
