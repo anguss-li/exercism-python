db81d0ead08abbc71f638e6d88ca6ff4d1f2087b
Angus Li <angusr.li02@gmail.com>
2021-07-11 21:56:04 +0800

    Added documentation to allergies

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,7 +1,6 @@
 from itertools import chain, combinations
 from typing import List
 
-
 class Allergies:
     allergens = {
         'eggs': 1,
@@ -15,12 +14,15 @@ class Allergies:
     }
 
     def __init__(self, score: int):
+        '''Record items that an individual is allergic to from allergens.'''
         self.score = score
 
     def allergic_to(self, item: str) -> bool:
+        '''Check whether the individual is allergic to item.'''
         return bool(self.score & Allergies.allergens[item])
 
     @property
     def lst(self) -> List[str]:
+        '''List all objects that the individual is allergic to.'''
         return [allergen for allergen in Allergies.allergens 
                 if self.allergic_to(allergen)]
