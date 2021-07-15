5e11185f2fb4eaa4ccd2a9b79c695df8874edeb3
Angus Li <angusr.li02@gmail.com>
2021-07-15 14:11:38 +0800

    Added twelve_days v3

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,45 +1,30 @@
 from typing import List
 
 
-ENDINGS = (
-    'first',
-    'second',
-    'third',
-    'fourth',
-    'fifth',
-    'sixth',
-    'seventh',
-    'eighth',
-    'ninth',
-    'tenth',
-    'eleventh',
-    'twelfth'
+VERSES = (
+    ("On the", "day of Christmas my true love gave to me:"),
+    ("first", "a Partridge in a Pear Tree."),
+    ("second", "two Turtle Doves, and"),
+    ("third", "three French Hens,"),
+    ("fourth", "four Calling Birds,"),
+    ("fifth", "five Gold Rings,"),
+    ("sixth", "six Geese-a-Laying,"),
+    ("seventh", "seven Swans-a-Swimming,"),
+    ("eighth", "eight Maids-a-Milking,"),
+    ("ninth", "nine Ladies Dancing,"),
+    ("tenth", "ten Lords-a-Leaping,"),
+    ("eleventh", "eleven Pipers Piping,"),
+    ("twelfth", "twelve Drummers Drumming,")
 )
 
-GIFTS = (
-    'a Partridge in a Pear Tree.',
-    'two Turtle Doves, and',
-    'three French Hens,',
-    'four Calling Birds,',
-    'five Gold Rings,',
-    'six Geese-a-Laying,',
-    'seven Swans-a-Swimming,',
-    'eight Maids-a-Milking,',
-    'nine Ladies Dancing,',
-    'ten Lords-a-Leaping,',
-    'eleven Pipers Piping,',
-    'twelve Drummers Drumming,'
-)
 
 def recite(start_verse: int, end_verse: int) -> List[str]:
     '''Print the Twelve Days of Christmas from the start verse to the end'''
 
-    def first_line(day: int) -> str:
-        return f'On the {ENDINGS[day]} day of Christmas my true love gave to me:'
-
     def gifts(day: int) -> str:
-        gifts = [first_line(day)]
-        gifts.extend([GIFTS[day] for day in reversed(range(0, day + 1))])
+        gifts = [phrase for phrase in VERSES[0]]
+        gifts.insert(-1, VERSES[day][0])
+        gifts.extend([VERSES[verse][1] for verse in range(day, 0, -1)])
         return " ".join(gifts)
 
-    return [gifts(day) for day in range(start_verse - 1, end_verse)]
+    return [gifts(day) for day in range(start_verse, end_verse + 1)]
