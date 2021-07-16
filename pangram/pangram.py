206c61f2c6fd2bec9398658b1a7a67cf40e1b9d4
Angus Li <angusr.li02@gmail.com>
2021-07-17 02:20:36 +0800

    Added pangram v3

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,9 +1,12 @@
-def is_pangram(sentence):
+from re import sub
+from string import ascii_lowercase
+
+ALPHABET = set(ascii_lowercase)
+
+
+def is_pangram(sentence: str) -> bool:
     '''
     sentence: string, phrase to be tested
     returns: True if sentence is a pangram, otherwise False
     '''
-    alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
-                'n','o','p','q','r','s','t','u','v','w','x','y','z'}
-    sentence_chars = {char for char in sentence.lower() if char in alphabet}
-    return sentence_chars == alphabet
+    return set(sub(r'[^a-z]', '', sentence.lower())) == ALPHABET
