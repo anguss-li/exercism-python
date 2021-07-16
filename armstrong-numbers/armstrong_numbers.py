72315474059503967038e3a770f472ab6a3aaeb1
Angus Li <angusr.li02@gmail.com>
2021-07-17 02:07:05 +0800

    Added armstrong_numbers v2

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -3,9 +3,6 @@ def is_armstrong_number(number):
     number: int, number to be tested for narcissism
     returns: True if number is a narcissistic/Armstrong number, False otherwise
     '''
-    powered_sum = 0
     digits = str(number)
-    digits_len = len(digits)
-    for digit in digits:
-        powered_sum += int(digit) ** digits_len
-    return powered_sum == number
+    digits_num = len(digits)
+    return sum(int(digit) ** digits_num for digit in digits) == number
