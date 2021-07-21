8612e34dbd35d79c8d45025c6fb052f619c5f918
Angus Li <angusr.li02@gmail.com>
2021-07-21 23:34:59 +0800

    Added scrabble_score v2

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,11 +1,11 @@
 SCORES = {
-    ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
-    ("D", "G"): 2,
-    ("B", "C", "M", "P"): 3,
-    ("F", "H", "V", "W", "Y"): 4,
-    ("K"): 5,
-    ("J", "X"): 8,
-    ("Q", "Z"): 10
+    **{letter: 1 for letter in "AEIOULNRST"},
+    **{letter: 2 for letter in "DG"},
+    **{letter: 3 for letter in "BCMP"},
+    **{letter: 4 for letter in "FHVWY"},
+    **{letter: 5 for letter in "K"},
+    **{letter: 8 for letter in "JX"},
+    **{letter: 10 for letter in "QZ"},
 }
 
 
