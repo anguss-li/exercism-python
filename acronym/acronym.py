580d722731fbc437e2ebd5f8a0b9625e93b2f1c3
Angus Li <angusr.li02@gmail.com>
2021-07-29 02:38:43 +0800

    Added acronym v2

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,6 +1,6 @@
+from re import findall
+
+
 def abbreviate(words: str) -> str:
     '''Returns acronym in capital letters of sentence.'''
-    acronym = ''
-    for word in words.upper().translate(words.maketrans('-_', '  ')).split():
-        acronym += word[0]
-    return acronym
+    return ''.join(word[0].upper() for word in findall(r"[a-zA-Z']+", words))
