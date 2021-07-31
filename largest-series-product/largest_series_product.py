10f1ac087b405a7a9a1f382b43e0fb84433e67d3
Angus Li <angusr.li02@gmail.com>
2021-08-01 01:11:28 +0800

    Added largest_series_product v1

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,13 @@
+from math import prod
+
+
+def largest_product(series: str, size: int) -> int:
+    '''Find largest product of a substring of digits in series of given size.'''
+    if size < 0:
+        raise ValueError('Size must be positive.')
+    elif size > (length := len(series)):
+        raise ValueError('Size cannot be larger than series.')
+    elif length == 0 or size == 0:
+        return 1
+    return max(prod(int(series[i+n]) for n in range(size))
+               for i in range(length-size+1))
