07f1e83a6902b63c006256c9c43ad268f929bd8c
Angus Li <angusr.li02@gmail.com>
2021-07-31 23:22:00 +0800

    Removed unnecessary copying of value in factors

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -3,12 +3,11 @@ from typing import List
 
 def factors(value: int) -> List[int]:
     '''Find the prime factors which can be multiplied to produce value'''
-    value_copy = value
     factors = []
-    for number in range(2, value + 1):
-        while value_copy % number == 0:
-            value_copy //= number
+    for number in range(2, value+1):
+        while value % number == 0:
+            value //= number
             factors.append(number)
-        if value_copy == 1:
+        if value == 1:
             break
     return factors
