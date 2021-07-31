7a2d8a5bf185d61d2269aa655f3d54ec3b65598a
Angus Li <angusr.li02@gmail.com>
2021-07-31 23:51:13 +0800

    Minor style changes for two_fer

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,3 +1,3 @@
 def two_fer(name: str = 'you') -> str:
-    '''Print "One for X, one for me." where X is name ('you' by default).'''
+    '''Print 'One for X, one for me.' where X is name ('you' by default).'''
     return f'One for {name}, one for me.'
