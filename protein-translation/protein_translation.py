77fc8c14cccc5573ebdffe0991b94034af930e37
Angus Li <angusr.li02@gmail.com>
2021-07-31 23:10:35 +0800

    Added protein_translation v2

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,19 +1,21 @@
-def proteins(strand):
-    '''
-    strand: string, chain of codons to be converted into a protein sequence
-    returns: list of strings, see README
-    '''
-    codon_indices = range(3, len(strand) + 1, 3)
-    codon_dict = {'AUG':'Methionine', 'UUU':'Phenylalanine',
-                  'UUC':'Phenylalanine', 'UUA':'Leucine', 'UUG':'Leucine', 
-                  'UCU':'Serine', 'UCC':'Serine', 'UCA':'Serine',
-                  'UCG':'Serine', 'UAU':'Tyrosine', 'UAC':'Tyrosine', 
-                  'UGU':'Cysteine', 'UGC':'Cysteine', 'UGG':'Tryptophan',
-                  'UAA':'STOP', 'UAG':'STOP', 'UGA':'STOP'}
-    protein_list = []
-    for index in codon_indices:
-        protein = codon_dict[strand[(index-3):index]]
-        if protein == 'STOP':
-            break
-        protein_list.append(protein)
-    return protein_list
+from typing import List
+
+
+def proteins(strand: str) -> List[str]:
+    '''Find which protein each codon in strand represents.'''
+    codons = {
+        **{codon: 'Methionine' for codon in ('AUG',)},
+        **{codon: 'Phenylalanine' for codon in ('UUU', 'UUC')},
+        **{codon: 'Leucine' for codon in ('UUA', 'UUG')},
+        **{codon: 'Serine' for codon in ('UCU', 'UCC', 'UCA', 'UCG')},
+        **{codon: 'Tyrosine' for codon in ('UAU', 'UAC')},
+        **{codon: 'Cysteine' for codon in ('UGU', 'UGC')},
+        **{codon: 'Tryptophan' for codon in ('UGG',)},
+        **{codon: 'STOP' for codon in ('UAA', 'UAG', 'UGA')},
+    }
+
+    proteins = [codons[strand[i-3:i]] for i in range(3, len(strand)+1, 3)]
+    if 'STOP' in proteins:
+        proteins = proteins[:proteins.index('STOP')]
+
+    return proteins
