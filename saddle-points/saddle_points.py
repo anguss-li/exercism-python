af3dd0c6671a3b68b1167a382d050b907048d560
Angus Li <angusr.li02@gmail.com>
2021-07-31 17:56:57 +0800

    Added saddle_points v5

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Dict, List
 
 
 def saddle_points(matrix: List[List]) -> List[Dict[str, int]]:
@@ -9,11 +9,7 @@ def saddle_points(matrix: List[List]) -> List[Dict[str, int]]:
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise ValueError("Rows must all be of equal length.")
 
-    points = set()
-
-    for i, row in enumerate(matrix):
-        for j, x in enumerate(row):
-            if x == max(row) and x == min(row[j] for row in matrix):
-                points.add((i+1, j+1))
-
-    return [{"row": point[0], "column": point[1]} for point in points]
+    return ({"row": i+1, "column": j+1}
+            for i, row in enumerate(matrix)
+            for j, x in enumerate(row)
+            if x == max(row) and x == min(row[j] for row in matrix))
