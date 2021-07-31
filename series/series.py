ecd8d60577c7f6d6965c253437e5885db25b5d83
Angus Li <angusr.li02@gmail.com>
2021-07-31 23:30:15 +0800

    Removed unnecessary use of join in series

--- series/series.py
+++ series/series.py
@@ -6,4 +6,4 @@ def slices(series: str, length: int) -> List[str]:
     if length > (digits := len(series)) or length < 1:
         raise ValueError(f'Cannot slice {digits} digits using {length} length')
 
-    return [''.join(series[i:i+length]) for i in range(digits-length+1)]
+    return [series[i:i+length] for i in range(digits-length+1)]
