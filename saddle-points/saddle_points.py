62d8cfc96d019a5bdf2fa176f97c96c82156cc1d
Angus Li <angusr.li02@gmail.com>
2021-07-31 18:21:52 +0800

    Changed saddle_points output to list

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -9,7 +9,7 @@ def saddle_points(matrix: List[List]) -> List[Dict[str, int]]:
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise ValueError("Rows must all be of equal length.")
 
-    return ({"row": i+1, "column": j+1}
+    return [{"row": i+1, "column": j+1}
             for i, row in enumerate(matrix)
             for j, x in enumerate(row)
-            if x == max(row) and x == min(row[j] for row in matrix))
+            if x == max(row) and x == min(row[j] for row in matrix)]
