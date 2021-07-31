7f90d2d937e721e1a2f48fc757241b3f2ab369fa
Angus Li <angusr.li02@gmail.com>
2021-08-01 00:10:42 +0800

    Restyled hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,4 +1,5 @@
-def distance(strand_a, strand_b):
+def distance(strand_a: str, strand_b: str) -> int:
+    '''Count the number of differing characters between the two strands.'''
     if len(strand_a) != len(strand_b):
-        raise ValueError("Strands must be of equal length.")
-    return sum(1 for char_a, char_b in zip(strand_a, strand_b) if char_a != char_b)
+        raise ValueError('Strands must be of equal length.')
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
