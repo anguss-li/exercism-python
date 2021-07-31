581d9ff79bfd9272a74d8e76249c8b849d35cae0
Angus Li <angusr.li02@gmail.com>
2021-08-01 00:14:14 +0800

    Restyled high_scores

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,22 +1,16 @@
-def latest(scores):
-    '''
-    scores: list of ints, scores from game
-    returns: last integer in scores
-    '''
+from typing import List
+
+
+def latest(scores: List[int]) -> int:
+    '''Find the latest score in scores.'''
     return scores[-1]
 
 
-def personal_best(scores):
-    '''
-    scores: list of ints, scores from game
-    returns: largest integer in scores
-    '''
+def personal_best(scores: List[int]) -> int:
+    '''Find the highest score in scores.'''
     return max(scores)
 
 
-def personal_top_three(scores):
-    '''
-    scores: list of ints, scores from game
-    returns: largest 3 integers in scores
-    '''
+def personal_top_three(scores: List[int]) -> List[int]:
+    '''Find the highest 3 scores in scores.'''
     return sorted(scores, reverse=True)[:3]
