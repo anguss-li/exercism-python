d8508464e19de3f72b2a6da9e33b614ada3f0e90
Angus Li <angusr.li02@gmail.com>
2021-07-31 22:48:21 +0800

    Improved phone_number documentation

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -2,9 +2,17 @@ from re import sub
 
 
 class PhoneNumber:
-    def __init__(self, number):
+    '''
+    A North American Numbering Plan (NANP) compliant telephone number.
+
+    Attributes:
+        number: a 10 digit phone number
+        area_code: 3 digit number representing (NANP) area
+    '''
+
+    def __init__(self, number: str):
         '''
-        number: string, number to be stored.
+        Clean number and check legality.
         '''
         number = sub(r'[^0-9]', '', number)
         if (number[0] != '1' and len(number) == 11) or (len(number) < 10):
@@ -14,9 +22,9 @@ class PhoneNumber:
             raise ValueError('1st/4th digit(s) invalid, must be from 2-9')
         self.area_code = self.number[0:3]
 
-    def pretty(self):
+    def pretty(self) -> str:
         '''
-        Returns: stylized self.number in format (NXX)-NXX-XXXX
+        Return stylized self.number in format (NXX)-NXX-XXXX.
         '''
         return '-'.join(['('+self.area_code+')',
                          self.number[3:6],
