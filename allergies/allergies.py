97d4f773b490176ef59e5f19c77d2dc16d187b58
Angus Li <angusr.li02@gmail.com>
2021-07-31 21:47:23 +0800

    Turned Allergies to dataclass in allergies

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,31 +1,34 @@
+from dataclasses import dataclass
 from typing import List
 
-class Allergies:
-    allergens = {
-        'eggs': 1,
-        'peanuts': 2,
-        'shellfish': 4,
-        'strawberries': 8,
-        'tomatoes': 16,
-        'chocolate': 32,
-        'pollen': 64,
-        'cats': 128
-    }
+ALLERGENS = {
+    'eggs': 1,
+    'peanuts': 2,
+    'shellfish': 4,
+    'strawberries': 8,
+    'tomatoes': 16,
+    'chocolate': 32,
+    'pollen': 64,
+    'cats': 128
+}
 
-    def __init__(self, score: int):
-        '''Record items that an individual is allergic to from allergens.'''
-        self.score = score
+
+@dataclass
+class Allergies:
+    '''Record items that an individual is allergic to from allergens.'''
+    score: int
 
     def allergic_to(self, item: str) -> bool:
         '''Check whether the individual is allergic to item.'''
         # '&' returns whether the binary versions of two numbers both have '1'
-        # in the same digit. As all allergy scores are multiples of 2 (10 in 
+        # in the same digit. As all allergy scores are multiples of 2 (10 in
         # binary), this can be used to quickly check if a given multiple of 2
         # is in a given number.
-        return bool(self.score & Allergies.allergens[item])
+        return bool(self.score & ALLERGENS[item])
 
     @property
     def lst(self) -> List[str]:
         '''List all objects that the individual is allergic to.'''
-        return [allergen for allergen in Allergies.allergens 
+        return [allergen 
+                for allergen in ALLERGENS
                 if self.allergic_to(allergen)]
