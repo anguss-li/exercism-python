3a8c3b887fea0a5f1ae3394418f1247b9b75d9a6
Angus Li <angusr.li02@gmail.com>
2021-08-01 00:04:17 +0800

    Reordered robot_name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -5,12 +5,12 @@ from string import ascii_uppercase as letters
 class Robot:
     _names = {None, }
 
+    def __init__(self):
+        self.name = None
+        self.reset()
+
     def reset(self):
         while self.name in Robot._names:
             new_name = (choices(letters, k=2) + choices(range(0, 9), k=3))
             self.name = ''.join(str(char) for char in new_name)
         Robot._names.add(self.name)
-
-    def __init__(self):
-        self.name = None
-        self.reset()
