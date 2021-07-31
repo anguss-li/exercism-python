c0543781e538c6caba133798131a3e76796a5e83
Angus Li <angusr.li02@gmail.com>
2021-08-01 00:02:35 +0800

    Added documentation to difference_of_squares

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,13 @@
-def square_of_sum(number):
-    return ((number * (number + 1)) / 2) ** 2
+def square_of_sum(number: int) -> int:
+    '''Square the sum of the natural numbers up to the number given.'''
+    return ((number*(number+1))/2) ** 2
 
 
-def sum_of_squares(number):
-    return (number * (number + 1) * (2 * number + 1)) / 6
+def sum_of_squares(number: int) -> int:
+    '''Sum the squares of the natural numbers up to the number given.'''
+    return (number*(number+1)*(2*number+1)) / 6
 
 
-def difference_of_squares(number):
+def difference_of_squares(number: int) -> int:
+    '''Subtract the sum of squares for number given by the square of its sum.'''
     return square_of_sum(number) - sum_of_squares(number)
