8f810f22ea7f39b288a7e27623d300ddd9e36477
Angus Li <angusr.li02@gmail.com>
2021-07-31 23:16:59 +0800

    Minor style change in protein_translation

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -3,16 +3,14 @@ from typing import List
 
 def proteins(strand: str) -> List[str]:
     '''Find which protein each codon in strand represents.'''
-    codons = {
-        **{codon: 'Methionine' for codon in ('AUG',)},
-        **{codon: 'Phenylalanine' for codon in ('UUU', 'UUC')},
-        **{codon: 'Leucine' for codon in ('UUA', 'UUG')},
-        **{codon: 'Serine' for codon in ('UCU', 'UCC', 'UCA', 'UCG')},
-        **{codon: 'Tyrosine' for codon in ('UAU', 'UAC')},
-        **{codon: 'Cysteine' for codon in ('UGU', 'UGC')},
-        **{codon: 'Tryptophan' for codon in ('UGG',)},
-        **{codon: 'STOP' for codon in ('UAA', 'UAG', 'UGA')},
-    }
+    codons = {**{codon: 'Methionine' for codon in ('AUG',)},
+              **{codon: 'Phenylalanine' for codon in ('UUU', 'UUC')},
+              **{codon: 'Leucine' for codon in ('UUA', 'UUG')},
+              **{codon: 'Serine' for codon in ('UCU', 'UCC', 'UCA', 'UCG')},
+              **{codon: 'Tyrosine' for codon in ('UAU', 'UAC')},
+              **{codon: 'Cysteine' for codon in ('UGU', 'UGC')},
+              **{codon: 'Tryptophan' for codon in ('UGG',)},
+              **{codon: 'STOP' for codon in ('UAA', 'UAG', 'UGA')}}
 
     proteins = [codons[strand[i-3:i]] for i in range(3, len(strand)+1, 3)]
     if 'STOP' in proteins:
