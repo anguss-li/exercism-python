7df67b2c9f8c394fe3598c9005d01737ac2677c4
Angus Li <angusr.li02@gmail.com>
2021-08-01 14:17:27 +0800

    Further restyling of phone_number

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -11,21 +11,19 @@ class PhoneNumber:
     '''
 
     def __init__(self, number: str):
-        '''
-        Clean number and check legality.
-        '''
+        '''Clean number and check legality.'''
         number = sub(r'[^0-9]', '', number)
         if (length := len(number)) < 10 or (number[0] != '1' and length == 11):
             raise ValueError('Invalid NANP phone number')
+
         self.number = number[length-10:length]
         if any(n == '0' or n == '1' for n in (self.number[0], self.number[3])):
             raise ValueError('1st/4th digit(s) invalid, must be from 2-9')
+
         self.area_code = self.number[0:3]
 
     def pretty(self) -> str:
-        '''
-        Return stylized self.number in format (NXX)-NXX-XXXX.
-        '''
+        '''Return stylized self.number in format (NXX)-NXX-XXXX.'''
         return '-'.join(['('+self.area_code+')',
                          self.number[3:6],
                          self.number[6:11]])
