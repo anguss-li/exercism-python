0043b5aeb6a047bb3787f78438b719df4e2ab6cc
Angus Li <angusr.li02@gmail.com>
2021-08-04 01:03:25 +0800

    Updated matrix documentation

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -3,23 +3,19 @@ from typing import List
 
 class Matrix:
     def __init__(self, matrix_string: str) -> None:
-        '''
-        A 2D array of integers, where each element represents a row. 
-        
-        Args:
+        """
+        A 2D integer array with each element representing a row. 
+
+        Parameters:
             matrix_string: numbers separated by spaces. "\n" is a row break.
-        '''
+        """
         self.matrix = [[int(entry) for entry in row.split()]
                        for row in matrix_string.splitlines()]
 
     def row(self, index: int) -> List[int]:
-        '''
-        Return the nth row of the matrix, where n == index.
-        '''
+        """Find the nth row of the matrix, where n == index."""
         return self.matrix[index - 1].copy()
 
     def column(self, index: int) -> List[int]:
-        '''
-        Return a list of the nth integer in each row, where n == index.
-        '''
+        """Find the nth integer in each row, where n == index."""
         return [row[index - 1] for row in self.matrix]
