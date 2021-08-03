25b26f3edf4f29609174cc7c1c7de9199cde6c28
Angus Li <angusr.li02@gmail.com>
2021-08-04 00:58:56 +0800

    Added kindergarten_garden v2

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from typing import List
 
 STUDENTS = ['Alice',
@@ -20,24 +21,28 @@ PLANTS = {'C': 'Clover',
 
 
 class Garden:
-    '''
-    Rows of plants taken care of by kindergarten students.
+    """
+    Rows of plants taken care of by kindergarten students. Each student is
+    responsible for the n-th column of the garden, where n is determined by
+    their name order and each column is 2 plants wide.
 
     Attributes:
         diagram: type of plant in each position in the rows
         students: students taking care of the plants
-    '''
+    """
 
     def __init__(self, diagram: str, students: List[str] = STUDENTS):
-        '''Clean inputs'''
-        self.diagram = diagram.splitlines()
+        """Determine which plants each student takes care of."""
+        diagram = diagram.splitlines()
+        assert all(len(row) == (row_len := len(diagram[0])) for row in diagram)
+
         self.students = sorted(students)
+        columns = (chain.from_iterable((row[n], row[n+1]) for row in diagram)
+                   for n in range(0, row_len, 2))
+
+        self.ownership = {student: [PLANTS[plant] for plant in column]
+                          for student, column in zip(self.students, columns)}
 
     def plants(self, student: str) -> List[str]:
-        '''Return plants student is responsible for'''
-        n = self.students.index(student) * 2
-        seeds = (self.diagram[0][n],
-                 self.diagram[0][n+1],
-                 self.diagram[1][n],
-                 self.diagram[1][n+1])
-        return [PLANTS[seed] for seed in seeds]
+        """Return the plants a student is responsible for."""
+        return self.ownership[student]
