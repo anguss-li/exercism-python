ea7accb3f89b7c37e5f646c6d20a4763581cd4d8
Angus Li <angusr.li02@gmail.com>
2021-08-05 14:55:23 +0800

    Added grade_school v1

---
+++ grade-school/grade_school.py
@@ -0,0 +1,21 @@
+class School:
+    def __init__(self):
+        """Store the names and grades of school students."""
+        self.students = {}
+
+    def add_student(self, name: str, grade: int) -> None:
+        """Add a student to a grade in the roster."""
+        school_grade = self.students.setdefault(grade, [])
+        school_grade.append(name)
+        school_grade.sort()
+
+    def roster(self) -> list:
+        """Find all students in the school regardless of grade."""
+        roster = [student
+                  for grade in sorted(self.students)
+                  for student in self.students[grade]]
+        return roster if roster else []
+
+    def grade(self, grade_number: int):
+        """Find all students in a particular grade."""
+        return self.students.setdefault(grade_number, [])
