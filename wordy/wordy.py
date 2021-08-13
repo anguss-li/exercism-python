49a5617457fda9e5bbb71128554a6bf420d2b086
Angus Li <angusr.li02@gmail.com>
2021-08-13 11:57:09 +0800

    Added wordy v1

---
+++ wordy/wordy.py
@@ -0,0 +1,38 @@
+from re import split
+from operator import add, sub, mul, floordiv
+
+OPERATIONS = {
+    'plus': add,
+    'minus': sub,
+    'multiplied by': mul,
+    'divided by': floordiv
+}
+
+
+def answer(question: str) -> int:
+    """Calculate the answer to a simple maths word problem."""
+    phrases = split(r' (?!\bby\b)', question.replace('?', ''))
+    length = len(phrases)-1
+
+    if not any(word in OPERATIONS for word in phrases):
+        try:
+            return int(phrases[-1])
+        except TypeError:
+            raise ValueError('No numbers and/or operators in question.')
+
+    stack = []
+
+    for i, word in enumerate(phrases):
+        if word.isdigit() and i != length and phrases[i+1] not in OPERATIONS:
+            raise ValueError('Missing operation.')
+        elif word not in OPERATIONS:
+            continue
+
+        try:
+            a, b = stack[-1] if stack else int(phrases[i-1]), int(phrases[i+1])
+        except (TypeError, IndexError):
+            raise ValueError('Numbers not in valid order.')
+
+        stack.append(OPERATIONS[word](a, b))
+
+    return stack.pop()
